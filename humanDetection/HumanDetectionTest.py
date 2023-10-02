3f6226fe846fe9e86f34d7513313fb94a8ed8972
Szymon Dizewski <szymon.dizewski1@gmail.com>
2023-10-02 09:56:11 +0200

    Implemented NMS

--- humanDetection/HumanDetectionTest.py
+++ humanDetection/HumanDetectionTest.py
@@ -2,8 +2,6 @@
 import numpy as np
 import cv2
 
-
-
 np.set_printoptions(suppress=True)
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
@@ -25,16 +23,17 @@ def doIntersect(box1, box2):
     
     # Check if rectangles are above eachother
     # Check if rectangles are left to eachother
+    # If everything is fales then they are intersecting
     w = bottom0 < top1
     e = bottom1 < top0
     r = right0 < left1
     c = right1 < left0
-    print(w,e,r,c)
+    # print(w,e,r,c)
     if bottom0 < top1 or bottom1 < top0 or right0 < left1 or right1 < left0:
         
         return False
 
-    print('INTERSECT')
+    # print('INTERSECT')
     return True
 
 # If there are boxes intersecting chose one with highest coefficient
@@ -48,6 +47,64 @@ def NMS(coefficients):
             index = i
         
     return index
+
+# Malisiewicz et al.
+def non_max_suppression_fast(boxes, overlapThresh):
+    # if there are no boxes, return an empty list
+    if len(boxes) == 0:
+        return []
+    # if the bounding boxes integers, convert them to floats --
+    # this is important since we'll be doing a bunch of divisions
+    if boxes.dtype.kind == "i":
+        boxes = boxes.astype("float")
+    # initialize the list of picked indexes    
+    pick = []
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:,0]
+    y1 = boxes[:,1]
+    x2 = boxes[:,2]
+    y2 = boxes[:,3]
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+        # compute the width and height of the bounding box
+        
+        w = np.array(xx2 - xx1 + 1)
+        h = np.array(yy2 - yy1 + 1)
+
+
+        print(xx2, xx1)
+
+        print('-----')
+        print(w)
+        print(h)
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+        # print(f'{w} * {h} / {area[idxs[:last]]}')
+        print(f'overlap: {overlap}')
+        idxs = np.delete(idxs, np.concatenate(([last],
+            np.where(overlap > overlapThresh)[0])))
+    # return only the bounding boxes that were picked using the
+    # integer data type
+
+ 
+    return boxes[pick].astype("int")
 
 
 
@@ -67,18 +124,20 @@ while(True):
     
     highestCoefficientIndex = 0
 
-    if len(boxes) > 1:
+    # if len(boxes) > 1:
 
         # print(len(boxes))
-        if doIntersect(boxes[0], boxes[1]):
+        # if doIntersect(boxes[0], boxes[1]):
             
-            highestCoefficientIndex = NMS(weights)
-            boxes= np.array([boxes[highestCoefficientIndex]])
+    boxes = non_max_suppression_fast(boxes, 0.4)
+            
+            # boxes= np.array([boxes[highestCoefficientIndex]])
+            
     
     #Debugg
-    if len(boxes) > 1:
-        print('Amount of boxes ', len(boxes))
-        print(boxes, weights, '\n')
+    # if len(boxes) > 1:
+        # print('Amount of boxes ', len(boxes))
+    #     print(boxes, weights, '\n')
         
 
 
@@ -86,11 +145,14 @@ while(True):
 
     boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
     
+    x = 0
     for (xA, yA, xB, yB) in boxes:
         # display the detected boxes in the colour picture
         cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
         cv2.putText(frame, str(x), (xA, yA-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        x +=1
     # Display the resulting frame
+    
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
