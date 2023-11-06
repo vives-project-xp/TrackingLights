f1fdfcd808d48ba7d5ecc50e66044bc9c67db063
Szymon Dizewski <szymon.dizewski1@gmail.com>
2023-11-06 09:27:06 +0100

    Fix spacing in loops

--- TrackingSoftware/Lights.py
+++ TrackingSoftware/Lights.py
@@ -35,31 +35,31 @@ class Lights:
             group_index += 1
             pixels_group.append([])
     
-        for group in pixels_group:
+    for group in pixels_group:
 
-          if(len(group) < 1):
-            break
-          
-          # Initialize values for image processing
-          first_pixel = group[0]
-          last_pixel = group[len(group)-1]
+      if(len(group) < 1):
+        break
+      
+      # Initialize values for image processing
+      first_pixel = group[0]
+      last_pixel = group[len(group)-1]
 
-          # Divide by 6 > each segment of leds = 6leds
-          first_pixel = int(group[0]/6)
-          last_pixel = int(group[len(group)-1]/6)
+      # Divide by 6 > each segment of leds = 6leds
+      first_pixel = int(group[0]/6)
+      last_pixel = int(group[len(group)-1]/6)
 
-          #Adjust pixels color with fade effect
-          self.lightsJson["seg"]["i"].append(first_pixel-3)
-          self.lightsJson["seg"]["i"].append(last_pixel+3)
-          self.lightsJson["seg"]["i"].append("C00000")
+      #Adjust pixels color with fade effect
+      self.lightsJson["seg"]["i"].append(first_pixel-3)
+      self.lightsJson["seg"]["i"].append(last_pixel+3)
+      self.lightsJson["seg"]["i"].append("C00000")
 
-          self.lightsJson["seg"]["i"].append(first_pixel-1)
-          self.lightsJson["seg"]["i"].append(last_pixel+1)
-          self.lightsJson["seg"]["i"].append("F50000")
+      self.lightsJson["seg"]["i"].append(first_pixel-1)
+      self.lightsJson["seg"]["i"].append(last_pixel+1)
+      self.lightsJson["seg"]["i"].append("F50000")
 
-          self.lightsJson["seg"]["i"].append(first_pixel)
-          self.lightsJson["seg"]["i"].append(last_pixel)
-          self.lightsJson["seg"]["i"].append("FF0000")
+      self.lightsJson["seg"]["i"].append(first_pixel)
+      self.lightsJson["seg"]["i"].append(last_pixel)
+      self.lightsJson["seg"]["i"].append("FF0000")
 
     return pixels_group
 
