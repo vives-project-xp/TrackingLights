796c1ec68f93dcdead81c622a7be4ca3aeb88b18
Szymon Dizewski <szymon.dizewski1@gmail.com>
2023-11-06 12:20:32 +0100

    changed video detection to default preset

--- TrackingSoftware/TrackingLight.py
+++ TrackingSoftware/TrackingLight.py
@@ -29,30 +29,7 @@ switcher = {
 }
 
 while(True): 
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    time.sleep(0.03)
-    # find best resolution
-    width = 600 # *3
-    height = 360 # *3
 
-    #Resize frame
-    frame = cv2.resize(frame, (width, height))
-
-    #Apply background subtraction
-    fgmask = fgbg.apply(frame, None, 0) 
-
-    #Blur out the edges
-    gray_frame = cv2.GaussianBlur(fgmask, (21,21), 0)  
-
-    # we will assign grayFrame to initalState if is none  
-    if initialState is None:  
-        initialState = gray_frame  
-        continue  
-
-    thresh_frame = cv2.threshold(gray_frame, 100, 255, cv2.THRESH_BINARY)[1]  
-    thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)  
-        
     #Checking two heights for better detection
     baseLineHeight = 215
     headLineHeight = 181
@@ -63,49 +40,85 @@ while(True):
     #Initialize list for detected pixels
     pixels = []
 
-    # check for white pixels on line (moving objects)
-    # and draw rectangle at this place
+    # if default preset: start video and
+    # and change lights if detected movement
     if(mqtt_controller_preset == 0):
+
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        time.sleep(0.03)
+        # find best resolution
+        width = 600 # *3
+        height = 360 # *3
+
+        #Resize frame
+        frame = cv2.resize(frame, (width, height))
+
+        #Apply background subtraction
+        fgmask = fgbg.apply(frame, None, 0) 
+
+        #Blur out the edges
+        gray_frame = cv2.GaussianBlur(fgmask, (21,21), 0)  
+
+        # we will assign grayFrame to initalState if is none  
+        if initialState is None:  
+            initialState = gray_frame  
+            continue  
+
+        thresh_frame = cv2.threshold(gray_frame, 100, 255, cv2.THRESH_BINARY)[1]  
+        thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)  
+            
+
         for i in range(0,width, 6):
             if thresh_frame[baseLineHeight][i] == 255 or thresh_frame[headLineHeight][i] == 255:
                 cv2.rectangle(frame, (i-3,baseLineHeight-3), (i+3,baseLineHeight+3), [34,0,255] ,-1)
                 #add detected pixels to list to be later grouped up
                 pixels.append(i)
-        #Send detected pixels list to grouping function
-        pixels_group = lights.groupingPixels(pixels)
 
+        #Send detected pixels list to grouping function
+        pixels_group = lights.groupingPixels(pixels, mqtt_controller.getInput())
+
+
+
+        #Send new json to mqtt controller
         switcher[mqtt_controller_preset](lights.getJson())
+
+        
+
+        #Draw green rectangles on image to see how detection works
+        for group in pixels_group:
+            if(len(group) < 1):
+                break
+            # Initialize values for image processing
+            first_pixel = group[0]
+            last_pixel = group[len(group)-1]
+
+            cv2.rectangle(frame, (first_pixel, 205), (last_pixel, 225), (0,255,0), 2)
+
+
+
+        # draw guideline which pixels are checked
+        cv2.line(frame, (0,baseLineHeight), (width,baseLineHeight), (0,255,0),thickness=1)
+        cv2.line(frame, (0,headLineHeight), (width,headLineHeight), (0,255,0),thickness=1)
+
+        # Draw guideline on the threshold frame as well
+        cv2.line(thresh_frame, (0,baseLineHeight), (width,baseLineHeight), (255,255,255),thickness=1)
+        cv2.line(thresh_frame, (0,headLineHeight), (width,headLineHeight), (255,255,255),thickness=1)
+
+        cv2.imshow('frame', frame)
+        cv2.imshow('threshold', thresh_frame)
+        cv2.imshow('backgroundDiff', fgmask)
+
+        # Move windows so they are properly placed
+        cv2.moveWindow('frame', 100,100)
+        cv2.moveWindow('threshold', 700,100)
+        cv2.moveWindow('backgroundDiff', 700,560)
+
     else:
         switcher[mqtt_controller_preset]()
 
 
-    #Draw green rectangles on image to see how detection works
-    for group in pixels_group:
-        if(len(group) < 1):
-            break
-        # Initialize values for image processing
-        first_pixel = group[0]
-        last_pixel = group[len(group)-1]
-
-        cv2.rectangle(frame, (first_pixel, 205), (last_pixel, 225), (0,255,0), 2)
-
-
-    # draw guideline which pixels are checked
-    cv2.line(frame, (0,baseLineHeight), (width,baseLineHeight), (0,255,0),thickness=1)
-    cv2.line(frame, (0,headLineHeight), (width,headLineHeight), (0,255,0),thickness=1)
-
-    # Draw guideline on the threshold frame as well
-    cv2.line(thresh_frame, (0,baseLineHeight), (width,baseLineHeight), (255,255,255),thickness=1)
-    cv2.line(thresh_frame, (0,headLineHeight), (width,headLineHeight), (255,255,255),thickness=1)
-
-    cv2.imshow('frame', frame)
-    cv2.imshow('threshold', thresh_frame)
-    cv2.imshow('backgroundDiff', fgmask)
-
-    # Move windows so they are properly placed
-    cv2.moveWindow('frame', 100,100)
-    cv2.moveWindow('threshold', 700,100)
-    cv2.moveWindow('backgroundDiff', 700,560)
+    
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
